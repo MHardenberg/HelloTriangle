e22de68df7aca9a4e635e276944795ad8f767566
Michel Hardenberg <MHardenberg@proton.me>
2024-04-19 00:00:26 +0200

    setting up opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
+#include "../include/glad/include/glad/gl.h"
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+#define projectName "Hello Triangle!!"
 
 int main () {
-
     std::cout << "Hello triangle!!" << std::endl;
+
+
+    // init glfw and use version 3.3, with OPENGL, OPENGL_CORE
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    // create window
+    GLFWwindow* window = glfwCreateWindow(800, 600, projectName, nullptr, 
+            nullptr);
+    if (window ==  nullptr) {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    // context of current thread
+    glfwMakeContextCurrent(window);
+
+
+    // glad setup
+    //if (!gladLoadGLLoader) // fix this? wrong import??
     return 0;
 }
