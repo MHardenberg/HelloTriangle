373d0636016ed78c455bda3c4443f1e003983b10
Michel Hardenberg <MHardenberg@proton.me>
2024-04-26 14:28:22 +0200

    resetup glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,39 @@
-#include "window.hpp"
-#include <iostream>
+#include <GLFW/glfw3.h>
+#include <memory>
+
+int main(void) {
+
+    /* Initialize the library */
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    auto glfwWindow* = glfwCreateWindow(640, 480, "Hello World",
+            glfwGetPrimaryMonitor(),
+            NULL) 
+        );
 
 
-int main () {
-    std::cout << "Hello triangle!!" << std::endl;
-    Window window;
-    window.render();
+    if (!*glfwWindow) {
+        glfwTerminate();
+        return -1;
+    }
 
+    /* Make the window's context current */
+    glfwMakeContextCurrent(*glfwWindow);
 
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(*glfwWindow)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Swap front and back buffers */
+        glfwSwapBuffers(*glfwWindow);
 
-    
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
 
-
-
-    // clean up
+    glfwTerminate();
     return 0;
 }
