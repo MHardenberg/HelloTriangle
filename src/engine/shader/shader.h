0a5de5b894b4b0df004171010f27d5a01a82ed7b
Michel Hardenberg <MHardenberg@proton.me>
2024-04-28 08:33:10 +0200

    added fragment shader

--- src/engine/shader/shader.h
+++ src/engine/shader/shader.h
@@ -9,7 +9,8 @@
 
 namespace shader {
     struct GLSource {
-        // This is the siuce code for various shaders. It is in ope
+// This is the source code for various shaders. It is in OpenGL SHader lang.
+// These have to be compiled and liked into a shader program.
         const char *vertexShader = 
             "#version 460 core\n"
             "layout (location = 0) in vec3 aPos;\n"
@@ -17,8 +18,16 @@ namespace shader {
             "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
             "}\0";
 
+
+// Colors in computer graphics are represented as an array of 4 values:
+// the red, green, blue and alpha (opacity) component, commonly abbreviated to 
+// RGBA. When defining a color in OpenGL or GLSL we set the strength of each
+// component to a value between 0.0 and 1.0. If, for example, we would set red
+// to 1.0 and green to 1.0 we would get a mixture of both colors and get the 
+// color yellow.
+
         const char* fragmentShader =
-            "#version 330 core\n"
+            "#version 460 core\n"
             "out vec4 FragColor\n;"
             "void main() {\n"
             "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
